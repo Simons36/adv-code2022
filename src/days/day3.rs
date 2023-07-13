6ffc83f6caef75ab177a68b5169ebe7552064821
Simão Silva <email@simaosilva.com>
2023-07-13 15:36:53 +0100

    implemented day 3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -1,13 +1,13 @@
-use crate::util::{self, read_lines};
+use crate::util::read_lines;
 
 pub fn function() -> Result<[String; 2], String>{
     
     //part 1
     let vect1 = read_lines("input/input3.txt");
 
-    let mut total_sum = 0;
+    let mut total_sum1 = 0;
 
-    for str in vect1{
+    for str in &vect1{
         let str_size = str.len();
 
         let part_size = str_size / 2;
@@ -30,14 +30,64 @@ pub fn function() -> Result<[String; 2], String>{
                 }
             }
         }
-
+        
         if char_equal.is_ascii_uppercase(){
-            total_sum += (char_equal as u32) - 38;
+            total_sum1 += (char_equal as u32) - 38;
         }else{
-            total_sum += (char_equal as u32) - 96;
+            total_sum1 += (char_equal as u32) - 96;
         }
+        
+    }
+    
+    
+    //part 2
+    let mut group_vec: Vec<String> = Vec::new();
+    let mut total_sum2 = 0;
 
+    for str in &vect1{
+        group_vec.push(str.to_string());
+
+        if group_vec.len() == 3{
+            let c = get_letter_equal(&group_vec);
+
+            if c.is_ascii_uppercase(){
+                total_sum2 += (c as u32) - 38;
+            }else{
+                total_sum2 += (c as u32) - 96;
+            }
+
+            group_vec.clear();
+        }
     }
 
-    Ok([total_sum.to_string(), "not implemented yet".to_string()])
+
+    Ok([total_sum1.to_string(), total_sum2.to_string()])
+}
+
+fn get_letter_equal(group_vector : &Vec<String>) -> char{
+
+    let chars1 = group_vector.get(0).map(|string| string.chars()).unwrap_or_else(|| "".chars());
+    let chars2 : Vec<char> = group_vector.get(1).map(|string| string.chars()).unwrap_or_else(|| "".chars()).collect();
+    let chars3 : Vec<char> = group_vector.get(2).map(|string| string.chars()).unwrap_or_else(|| "".chars()).collect();
+
+    let mut char_return = '\0';
+
+
+'outer: for c1 in chars1{
+            let chars2clone = chars2.clone();
+            for c2 in chars2clone{
+                if c1 == c2{
+                    let chars3clone = chars3.clone(); 
+                    for c3 in chars3clone{
+                        if c3 == c1{
+                            char_return = c3;
+                            break 'outer;
+                        }
+                    }
+                }
+            }
+        }
+
+        return char_return;
+    
 }
